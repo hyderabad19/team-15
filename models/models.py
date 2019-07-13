c3870093319ad6ffba1e5a28875883e7dfee4f42
Sreemae <sreemai.k075@gmail.com>
2019-07-14 04:52:49 +0530

    Update models.py

--- models/models.py
+++ models/models.py
@@ -18,3 +18,14 @@ class Mandal(models.Model):
 
     def _str_(self):
         return self.name
+
+class User(models.Model):
+    name = models.CharField(max_length=20,default='sai')
+    email = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    contact = models.IntegerField(default=0)
+    type = models.CharField(max_length = 20,choices = type,default='Incharge')
+    approved = models.BooleanField(default=False)
+
+    def _str_(self):
+        return self.email
