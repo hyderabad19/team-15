cd4ae852d3da5a741e82182804a2688d5be7f606
Sreemae <sreemai.k075@gmail.com>
2019-07-14 04:55:51 +0530

    Update models.py

--- models/models.py
+++ models/models.py
@@ -29,3 +29,43 @@ class User(models.Model):
 
     def _str_(self):
         return self.email
+class Cluster(models.Model):
+    cid = models.IntegerField(default=0)
+    name = models.CharField(max_length=10)
+
+    def _str_(self):
+        return self.cid
+
+class School(models.Model):
+    name = models.CharField(max_length=20)
+    address = models.CharField(max_length=20,default='new')
+    verified = models.BooleanField(default=False)
+    mandal = models.ForeignKey(Mandal,on_delete=models.CASCADE)
+    district = models.ForeignKey(District,on_delete=models.CASCADE)
+
+
+    def _str_(self):
+        return self.name
+
+class ClusterSchool(models.Model):
+    school = models.ForeignKey(School,on_delete=models.CASCADE)
+    cluster = models.ForeignKey(Cluster,on_delete=models.CASCADE)
+
+    def _str_(self):
+        return self.id
+
+class TypeResource(models.Model):
+    type = models.CharField(max_length=20)
+
+    def _str_(self):
+        return self.type
+
+class Resource(models.Model):
+    school_name = models.CharField(max_length=30)
+    resource_name = models.CharField(max_length=30)
+    type = models.ForeignKey(TypeResource,on_delete=models.CASCADE)
+    verify = models.BooleanField(default=False)
+    assigned = models.BooleanField(default=False)
+
+    def _str_(self):
+        return self.school_name
